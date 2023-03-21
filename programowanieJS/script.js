2d72b0b6866d9a62be435aa97657385003def891
kplmarcin <116274226+kplmarcin@users.noreply.github.com>
2023-03-21 15:03:51 +0100

    Zadanie 1 Javascript (niedokonczone)

---
+++ programowanieJS/script.js
@@ -0,0 +1,15 @@
+
+// // Zadanie 1
+var a=20;
+var b=10;
+let add=a+b;
+let sub=a-b;
+let mul=a*b;
+let div=a/b;
+let mod=a%b;
+console.log("add");
+console.log("sub");
+console.log("mul");
+console.log("div");
+console.log("mod");
+document.getElementById("add").innerHTML;
